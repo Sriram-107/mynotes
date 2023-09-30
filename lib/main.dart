a1cf001af38205c817142fa88917438b16dbd2b2
ASPIRESYS\sriram.ravisankar <sriram.ravisankar@aspiresys.com>
2023-09-30 22:44:57 +0530

    step-3

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/email_verify.dart';
 import 'package:mynotes/views/login.dart';
 import 'package:mynotes/views/register.dart';
+import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,25 +35,91 @@ class HomePage extends StatelessWidget {
               final userCred = FirebaseAuth.instance.currentUser;
               if (userCred != null) {
                 if (userCred.emailVerified) {
-                  print("Email is verified");
+                  return const NotesView();
                 } else {
                   return const EmailVerificationView();
                 }
               } else {
                 return const LoginView();
               }
-              // return const Text("Done");
-              //   print("User is Verified");
-              //   return const Text("Done");
-              // } else {
-              //   print("User is not verified");
-              //   return const EmailVerificationView();
-              // }
-              return const LoginView();
 
             default:
               return const CircularProgressIndicator();
           }
         });
   }
+}
+
+enum MenuActions { logout }
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+        actions: [
+          PopupMenuButton(
+            onSelected: (value) async {
+              // devtools.log(value.toString());
+              switch (value) {
+                case MenuActions.logout:
+                  final navigator = Navigator.of(
+                      context); // use the context before the await call.
+                  final showLogout = await showDialogBox(context);
+                  if (showLogout) {
+                    await FirebaseAuth.instance.signOut();
+                    navigator.pushNamedAndRemoveUntil("/login/", (_) => false);
+                  }
+                  devtools.log(showLogout.toString());
+                  break;
+              }
+            },
+            itemBuilder: (context) {
+              return const [
+                PopupMenuItem(
+                  value: MenuActions.logout,
+                  child: Text("Logout"),
+                ),
+              ];
+            },
+          )
+        ],
+      ),
+      body: const Text("Notes List"),
+    );
+  }
+}
+
+Future<bool> showDialogBox(BuildContext context) {
+  return showDialog<bool>(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: const Text("Sign Out"),
+      content: const Text("Are you sure you want to sign out?"),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(false);
+          },
+          child: const Text("Not yet!"),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(true);
+          },
+          child: const Text("Sign Out"),
+        ),
+      ],
+    ),
+  ).then((value) =>
+      value ??
+      false); // once the future is completed take the result and pass it to the callback function.
 }
